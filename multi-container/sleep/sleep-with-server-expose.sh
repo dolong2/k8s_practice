59d5a6926d5115f0117d51bf2623b9e9888d0e4b
dolong2 <k01066624566@gmail.com>
2025-08-27 23:30:39 +0900

    [add] 서버 컨테이너의 포트를 주시하는 서비스 로그 확인 예시 추가

--- multi-container/sleep/sleep-with-server-expose.sh
+++ multi-container/sleep/sleep-with-server-expose.sh
@@ -3,3 +3,6 @@ kubectl expose -n multi-container -f sleep-with-server.yaml --type LoadBalancer 
 
 # 서비스의 URL 출력
 kubectl get svc -n multi-container sleep -o jsonpath='http://{.status.loadBalancer.ingress[0].*}:8020'
+
+# 브라우저에서 출력된 URL에 접근후 서버 컨테이너의 로그 확인
+kubectl logs -n multi-container -l app=sleep -c server
