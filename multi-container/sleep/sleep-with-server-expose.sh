e59c9975c1961589c18d59fbc45f4fb49e59fe01
dolong2 <k01066624566@gmail.com>
2025-08-27 23:29:38 +0900

    [add] 서버 컨테이너의 포트를 주시하는 서비스 URL 출력 명령 추가

--- multi-container/sleep/sleep-with-server-expose.sh
+++ multi-container/sleep/sleep-with-server-expose.sh
@@ -1,2 +1,5 @@
 # 서버 컨테이너의 포트를 주시하는 서비스 생성
 kubectl expose -n multi-container -f sleep-with-server.yaml --type LoadBalancer --port 8020 --target-port 8080
+
+# 서비스의 URL 출력
+kubectl get svc -n multi-container sleep -o jsonpath='http://{.status.loadBalancer.ingress[0].*}:8020'
